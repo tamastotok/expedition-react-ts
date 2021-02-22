f2a7202c8d32f9a9c673ee8cd833585ae6fae91e
Yhunie <yhuniegaming@gmail.com>
2021-02-22 18:41:39 +0100

    style fix

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -5,7 +5,6 @@ import "./style.scss";
 export default function Share() {
    return (
       <div id="share">
-         <p>share page</p>
          <img src={share} alt="share-icon" />
       </div>
    );
